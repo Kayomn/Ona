26b8d02938555098aa1f7fd79d0071f2016a4f4b
Kayomn <kayomn@kayomn.net>
2020-09-21 18:31:32 +0100

    Reduce file handle usage of build script

--- build.py
+++ build.py
@@ -61,106 +61,112 @@ target_types = [
 ]
 
 def build(name: str) -> (bool, str):
+	def load_build_config(file_path: str) -> dict:
+		if (not path.exists(file_path)):
+			print("No build.json exists for", name)
+			exit(1)
+
+		with open(file_path) as file:
+			return json.load(file)
+
 	module_path = path.join(input_path, name)
+	build_config = load_build_config(module_path + ".json")
 
-	with open(module_path + ".json") as build_file:
-		build_config = json.load(build_file)
+	if (not "targetType" in build_config):
+		print("No target type specified in build.json for", name)
+		exit(1)
 
-		if (not "targetType" in build_config):
-			print("No target type specified in build.json for", name)
-			exit(1)
+	target_type = build_config["targetType"]
 
-		target_type = build_config["targetType"]
+	if (not target_type in target_types):
+		print("Invalid target type specified in build.json for", name)
+		exit(1)
 
-		if (not target_type in target_types):
-			print("Invalid target type specified in build.json for", name)
-			exit(1)
+	compilation_process_ids = []
+	object_paths = []
+	dependency_paths = []
+	needs_recompile = False
 
-		compilation_process_ids = []
-		object_paths = []
-		dependency_paths = []
-		needs_recompile = False
+	def compile_source(source_path: str, object_path: str) -> None:
+		print(source_path, "->", object_path)
 
-		def compile_source(source_path: str, object_path: str) -> None:
-			print(source_path, "->", object_path)
+		compilation_process_ids.append(Popen([
+			"clang++",
+			source_path,
+			("-o" + object_path),
+			"-c"
+		] + common_flags))
 
-			compilation_process_ids.append(Popen([
-				"clang++",
-				source_path,
-				("-o" + object_path),
-				"-c"
-			] + common_flags))
+	def to_object_path(source_path: str) -> str:
+		path_nodes = []
 
-		def to_object_path(source_path: str) -> str:
-			path_nodes = []
+		while (source_path):
+			split = path.split(source_path)
+			source_path = split[0]
 
-			while (source_path):
-				split = path.split(source_path)
-				source_path = split[0]
+			path_nodes.append(split[1])
 
-				path_nodes.append(split[1])
+		if (len(path_nodes)):
+			path_nodes[0] = (path.splitext(path_nodes[0])[0] + ".o")
 
-			if (len(path_nodes)):
-				path_nodes[0] = (path.splitext(path_nodes[0])[0] + ".o")
+			path_nodes.reverse()
 
-				path_nodes.reverse()
+			object_path = path.join(output_path, ".".join(path_nodes))
 
-				object_path = path.join(output_path, ".".join(path_nodes))
+			object_paths.append(object_path)
 
-				object_paths.append(object_path)
+			return object_path
 
-				return object_path
+		return ""
 
-			return ""
+	if ("dependencies" in build_config):
+		for dependency in build_config["dependencies"]:
+			if (not dependency in processed_dependencies):
+				build_result, dependency_path = build(dependency)
+				needs_recompile |= build_result
 
-		if ("dependencies" in build_config):
-			for dependency in build_config["dependencies"]:
-				if (not dependency in processed_dependencies):
-					build_result, dependency_path = build(dependency)
-					needs_recompile |= build_result
+				dependency_paths.append(dependency_path)
+				processed_dependencies.append(dependency)
 
-					dependency_paths.append(dependency_path)
-					processed_dependencies.append(dependency)
+	print("Building", (name + "..."))
 
-		print("Building", (name + "..."))
+	binary_path = target_type_namers[target_type](path.join(output_path, name))
+	header_path = (module_path + ".hpp")
 
-		binary_path = target_type_namers[target_type](path.join(output_path, name))
-		header_path = (module_path + ".hpp")
+	# A re-compilation is needed if the module header is newer than the output binary.
+	needs_recompile |= (
+		not path.exists(binary_path) or
+		(path.getmtime(header_path) > path.getmtime(binary_path))
+	)
 
-		# A re-compilation is needed if the module header is newer than the output binary.
-		needs_recompile |= (
-			not path.exists(binary_path) or
-			(path.getmtime(header_path) > path.getmtime(binary_path))
-		)
-
-		if (path.exists(module_path)):
-			if (needs_recompile):
-				# A dependency has changed so re-compile the entire module.
-				for file_name in listdir(module_path):
-					source_path = path.join(module_path, file_name)
-
-					compile_source(source_path, to_object_path(source_path))
-			else:
-				for file_name in listdir(module_path):
-					source_path = path.join(module_path, file_name)
-					object_path = to_object_path(source_path)
-
-					if (
-						(not path.exists(object_path)) or
-						(path.getmtime(source_path) > path.getmtime(object_path))
-					):
-						compile_source(source_path, object_path)
-
-						needs_recompile = True
-
+	if (path.exists(module_path)):
 		if (needs_recompile):
-			if (all((pid == 0) for pid in [pid.wait() for pid in compilation_process_ids])):
-				target_type_linkers[target_type](
-					name,
-					object_paths,
-					dependency_paths,
-					(build_config["libraries"] if "libraries" in build_config else [])
-				)
+			# A dependency has changed so re-compile the entire module.
+			for file_name in listdir(module_path):
+				source_path = path.join(module_path, file_name)
+
+				compile_source(source_path, to_object_path(source_path))
+		else:
+			for file_name in listdir(module_path):
+				source_path = path.join(module_path, file_name)
+				object_path = to_object_path(source_path)
+
+				if (
+					(not path.exists(object_path)) or
+					(path.getmtime(source_path) > path.getmtime(object_path))
+				):
+					compile_source(source_path, object_path)
+
+					needs_recompile = True
+
+	if (needs_recompile):
+		if (all((pid == 0) for pid in [pid.wait() for pid in compilation_process_ids])):
+			target_type_linkers[target_type](
+				name,
+				object_paths,
+				dependency_paths,
+				(build_config["libraries"] if "libraries" in build_config else [])
+			)
 
 	return needs_recompile, binary_path
 
