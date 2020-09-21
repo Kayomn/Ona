d54a8031829e8f2d44068ef81386771416e9c058
Kayomn <kayomn@kayomn.net>
2020-09-21 20:52:58 +0100

    Implement partial stubs for OpenGL version of graphics server backend

--- ona/engine/opengl.cpp
+++ ona/engine/opengl.cpp
@@ -1,10 +1,41 @@
 #include "ona/engine.hpp"
 
 #include <SDL2/SDL.h>
+#include <GL/glew.h>
 #include <GL/gl.h>
 
 namespace Ona::Engine {
+	struct Renderer {
+		uint32_t shaderProgramHandle;
+
+		MaterialLayout materialLayout;
+
+		VertexLayout vertexLayout;
+	};
+
+	struct Material {
+		ResourceId rendererId;
+
+		ResourceId shaderId;
+
+		uint32_t textureHandle;
+
+		Slice<uint8_t> uniformData;
+	};
+
+	struct Poly {
+		ResourceId rendererId;
+
+		uint32_t bufferHandle;
+
+		uint32_t vertexArrayHandle;
+
+		uint32_t vertexCount;
+	};
+
 	GraphicsServer * LoadOpenGl(String const & title, int32_t width, int32_t height) {
+		using Ona::Collections::Appender;
+
 		thread_local class OpenGlGraphicsServer extends GraphicsServer {
 			public:
 			uint64_t timeNow, timeLast;
@@ -12,6 +43,12 @@ namespace Ona::Engine {
 			SDL_Window * window;
 
 			void * context;
+
+			Appender<Renderer> renderers;
+
+			Appender<Material> materials;
+
+			Appender<Poly> polys;
 
 			~OpenGlGraphicsServer() override {
 				SDL_GL_DeleteContext(this->context);
@@ -53,6 +90,124 @@ namespace Ona::Engine {
 			void Update() override {
 				SDL_GL_SwapWindow(this->window);
 			}
+
+			ResourceId CreateShader(Chars const & sourceCode, ShaderError * error) override {
+				return 0;
+			}
+
+			ResourceId CreateRenderer(
+				ResourceId shaderId,
+				MaterialLayout const & materialLayout,
+				VertexLayout const & vertexLayout,
+				RendererError * error
+			) override {
+				return 0;
+			}
+
+			ResourceId CreatePoly(
+				ResourceId rendererId,
+				Slice<uint8_t const> const & vertexData,
+				PolyError * error
+			) override {
+				return 0;
+			}
+
+			ResourceId CreateMaterial(
+				ResourceId rendererId,
+				Slice<uint8_t const> const & materialData,
+				Image const & texture,
+				ResourceId shaderId,
+				MaterialError * error
+			) override {
+				if (this->renderers.At(rendererId).materialLayout.Validate(materialData)) {
+					Slice<uint8_t> uniformData = Ona::Core::Allocate(materialData.length);
+
+					if (uniformData) {
+						uint32_t textureHandle;
+
+						Ona::Core::CopyMemory(uniformData, materialData);
+						glCreateTextures(GL_TEXTURE_2D, 1, &textureHandle);
+
+						glTextureStorage2D(
+							textureHandle,
+							1,
+							GL_RGBA8,
+							texture.dimensions.x,
+							texture.dimensions.y
+						);
+
+						switch (glGetError()) {
+							case GL_NO_ERROR: {
+								if (texture.pixels) {
+									glTextureSubImage2D(
+										textureHandle,
+										0,
+										0,
+										0,
+										texture.dimensions.x,
+										texture.dimensions.y,
+										GL_RGBA,
+										GL_UNSIGNED_BYTE,
+										texture.pixels.pointer
+									);
+
+									if (glGetError() == GL_NO_ERROR) {
+										constexpr struct {
+											GLenum property;
+
+											int32_t value;
+										} settings[] = {
+											{GL_TEXTURE_MIN_FILTER, GL_LINEAR},
+											{GL_TEXTURE_MAG_FILTER, GL_LINEAR},
+											{GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE},
+											{GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE}
+										};
+
+										constexpr size_t settingsCount = (
+											sizeof(settings) / sizeof(settings[0])
+										);
+
+										for (size_t i = 0; i < settingsCount; i += 1) {
+											glTextureParameteri(
+												textureHandle,
+												settings[i].property,
+												settings[i].value
+											);
+
+											if (glGetError() != GL_NO_ERROR) {
+												if (error) (*error) = MaterialError::Server;
+
+												return 0;
+											}
+										}
+
+										ResourceId const id = static_cast<ResourceId>(
+											this->materials.Count()
+										);
+
+										if (this->materials.Append(Material{
+											rendererId,
+											shaderId,
+											textureHandle,
+											uniformData
+										})) return id;
+									} else if (error) (*error) = MaterialError::BadImage;
+								}
+							} break;
+
+							case GL_INVALID_VALUE: {
+								if (error) (*error) = MaterialError::BadImage;
+							} break;
+
+							default: {
+								if (error) (*error) = MaterialError::Server;
+							} break;
+						}
+					}
+				}
+
+				return 0;
+			}
 		} graphicsServer = {};
 
 		constexpr int32_t initFlags = SDL_INIT_EVERYTHING;
@@ -88,8 +243,9 @@ namespace Ona::Engine {
 				if (SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24) != 0) return nullptr;
 
 				graphicsServer.context = SDL_GL_CreateContext(graphicsServer.window);
+				glewExperimental = true;
 
-				if (graphicsServer.context) {
+				if (graphicsServer.context && (glewInit() == GLEW_OK)) {
 					glEnable(GL_DEPTH_TEST);
 
 					if (glGetError() == GL_NO_ERROR) {
