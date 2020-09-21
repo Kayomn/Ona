2c1375b748ec65ff4943f3b1a7a2354d80591e11
Kayomn <kayomn@kayomn.net>
2020-09-21 11:02:02 +0100

    Add collections package to project and some testing code

---
+++ source/collections/appender.hpp
@@ -0,0 +1,126 @@
+#ifndef COLLECTIONS_APPENDER_H
+#define COLLECTIONS_APPENDER_H
+
+#include "ona/core.hpp"
+
+namespace Ona::Collections {
+	using Ona::Core::Allocator;
+	using Ona::Core::Reallocate;
+	using Ona::Core::Slice;
+
+	template<typename Type> class Appender final {
+		Allocator * allocator;
+
+		size_t count;
+
+		Slice<Type> values;
+
+		public:
+		Appender() = default;
+
+		Appender(Allocator * allocator) : allocator{allocator} { }
+
+		~Appender() {
+			for (auto & value : this->Values()) value.~Type();
+
+			if (this->allocator) {
+				this->allocator->Deallocate(reinterpret_cast<uint8_t *>(this->values.pointer));
+			} else {
+				Ona::Core::Deallocate(reinterpret_cast<uint8_t *>(this->values.pointer));
+			}
+		}
+
+		Allocator * AllocatorOf() {
+			return this->allocator;
+		}
+
+		Type * Append(Type const & value) {
+			if (this->count >= this->values.length) {
+				if (!this->Reserve(this->count ? this->count : 2)) {
+					// Allocation failure.
+					return nullptr;
+				}
+			}
+
+			Type * bufferIndex = (this->values.pointer + this->count);
+			this->count += 1;
+			(*bufferIndex) = value;
+
+			return bufferIndex;
+		}
+
+		Slice<Type> AppendAll(Slice<Type> const & elements) {
+			if (this->Reserve(elements.length)) {
+				Slice<Type> range = this->values.Sliced(this->count, (this->count + values.length));
+
+				for (size_t i = 0; i < range.length; i += 1) range[i] = elements[i];
+
+				this->count += elements.length;
+
+				return range;
+			}
+
+			return Slice<Type>{};
+		}
+
+		Type & At(size_t index) {
+			return this->values(index);
+		}
+
+		Type const & At(size_t index) const {
+			return this->values(index);
+		}
+
+		size_t Capacity() const {
+			return this->values.length;
+		}
+
+		void Clear() {
+			for (auto & value : this->Values()) value.~Type();
+
+			this->count = 0;
+		}
+
+		bool Compress() {
+			if (this->allocator) {
+				this->values = this->allocator->Reallocate(
+					this->values.pointer,
+					(sizeof(Type) * this->count)
+				).template As<Type>();
+			} else {
+				this->elements = Reallocate(
+					this->values.pointer,
+					(sizeof(Type) * this->count)
+				).template As<Type>();
+			}
+
+			return (this->values.pointer != nullptr);
+		}
+
+		bool Reserve(size_t capacity) {
+			if (this->allocator) {
+				this->values = this->allocator->Reallocate(
+					reinterpret_cast<uint8_t *>(this->values.pointer),
+					(sizeof(Type) * (this->values.length + capacity)
+				)).template As<Type>();
+			} else {
+				this->values = Reallocate(
+					reinterpret_cast<uint8_t *>(this->values.pointer),
+					(sizeof(Type) * (this->values.length + capacity)
+				)).template As<Type>();
+			}
+
+			return (this->values.pointer != nullptr);
+		}
+
+		Slice<Type> Values() {
+			return this->values.Sliced(0, this->values.length);
+		}
+
+		Slice<Type const> Values() const {
+			return this->values.Sliced(0, this->values.length);
+		}
+	};
+}
+
+#endif
--- source/engine/app.cpp
+++ source/engine/app.cpp
@@ -1,12 +1,21 @@
 #include "engine/graphics.hpp"
+#include "collections/appender.hpp"
 
 using namespace Ona::Graphics;
+using namespace Ona::Collections;
 
 int main(int argv, char const * const * argc) {
 	GraphicsServer * graphics = LoadOpenGl(String::From("Ona"), 640, 480);
 
 	if (graphics) {
 		Events events = {};
+		Appender<char> test = {};
+
+		test.Append('H');
+		test.Append('e');
+		test.Append('l');
+		test.Append('l');
+		test.Append('o');
 
 		while (graphics->ReadEvents(events)) {
 			graphics->Clear();
