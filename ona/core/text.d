bf3aad47b384d6407c11151c68d4baab40c4185d
Kayomn <kayomn@kayomn.net>
2020-10-16 11:38:49 +0100

    Partially implement more text utility library features

--- ona/core/text.d
+++ ona/core/text.d
@@ -4,7 +4,103 @@ private import
 	ona.core.memory,
 	ona.core.os,
 	ona.core.types,
-	std.algorithm;
+	std.algorithm,
+	std.ascii;
+
+/**
+ * Generative state machine for extracting lexemes from textual data using a simple ruleset system.
+ */
+public struct Lexer {
+	/**
+	 * Source data to lex.
+	 */
+	const (char)[] source;
+
+	/**
+	 * Set of lexible symbol character tokens.
+	 */
+	const (char)[] symbols;
+
+	/**
+	 * Set of skippable characters that are ignored.
+	 */
+	const (char)[] skippable;
+
+	/**
+	 * Constructs a `Lexer` using `source` as the source data to lex, `symbols` as the set of symbol
+	 * tokens, and `skippable` as the characters that are ignored.
+	 *
+	 * Note that both `symbols` and `skippable` will delimit identifier tokens. For example, a
+	 * `Tokenizer` with a `symbols` value of `"."` and a `skippable` value of ` ` will tokenize
+	 * `"my.print value"` as `["my", ".", "print", "value"]`.
+	 */
+	@nogc
+	this(in char[] source, in char[] symbols, in char[] skippable) pure {
+		this.source = source;
+		this.symbols = symbols;
+		this.skippable = skippable;
+	}
+
+	/**
+	 * Checks if `c` is a skippable character.
+	 */
+	@nogc
+	bool isSkippable(in char c) pure const {
+		foreach (skip; this.skippable) if (c == skip) return true;
+
+		return false;
+	}
+
+	/**
+	 * Checks if `c` is a symbol token character.
+	 */
+	@nogc
+	bool isSymbol(in char c) pure const {
+		foreach (symbol; this.symbols) if (c == symbol) return true;
+
+		return false;
+	}
+
+	/**
+	 * Lexes `Lexer.source` and generatively produces each lexeme for `action` to consume according
+	 * to the ruleset specified in `Lexer.symbols` and `Lexer.skippable`.
+	 */
+	@nogc
+	int opApply(int delegate(in char[] token) @nogc action) const {
+		size_t cursor;
+
+		while (cursor < this.source.length) {
+			if (isWhite(this.source[cursor])) {
+				// Skip whitespace.
+				cursor += 1;
+			} else if (this.isSymbol(this.source[cursor])) {
+				// Handle single character symbols directly.
+				immutable tokenEnd = (cursor + 1);
+
+				if (action(this.source[cursor .. tokenEnd])) return 1;
+
+				cursor = tokenEnd;
+			} else {
+				// The smallest a token can be is 1 character.
+				size_t tokenEnd = cursor;
+
+				while (
+					(tokenEnd < this.source.length) &&
+					(!this.isSkippable(this.source[tokenEnd])) &&
+					(!this.isSymbol(this.source[tokenEnd]))
+				) {
+					tokenEnd += 1;
+				}
+
+				if (action(this.source[cursor .. tokenEnd])) return 1;
+
+				cursor = tokenEnd;
+			}
+		}
+
+		return 0;
+	}
+}
 
 /**
  * Reference-counted, UTF-8-encoded character sequence.
@@ -77,13 +173,15 @@ public struct String {
 	@nogc
 	private ubyte[] createBuffer(in uint size) return {
 		if (size > staticBufferSize) {
+			this.size = size;
 			this.data.dynamic = allocate(size_t.sizeof + clamp(size, 0, this.size.max)).ptr;
 
 			if (this.data.dynamic) {
 				this.refCounter() = 1;
-				this.size = size;
 
 				return (this.data.dynamic + size_t.sizeof)[0 .. this.size];
+			} else {
+				this.size = 0;
 			}
 		} else {
 			return this.data.static_[0 .. size];
