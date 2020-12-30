f0b51d8bd35c49c7fa01e621b74c19c069c8b176
Kayomn <kayomn@kayomn.net>
2020-12-30 04:00:38 +0000

    Fix clean script not removing generated asset files

--- clean.py
+++ clean.py
@@ -2,8 +2,16 @@
 
 from os import listdir, remove, path
 
+assets_path = "assets"
 output_path = "output"
 
+# Generated asset files.
+if (path.exists(assets_path)):
+	for filename in listdir(assets_path):
+		if (filename.endswith(".so") or filename.endswith(".dll")):
+			remove(path.join(assets_path, filename))
+
+# Engine binaries.
 if (path.exists(output_path)):
 	for filename in listdir(output_path):
 		remove(path.join(output_path, filename))
