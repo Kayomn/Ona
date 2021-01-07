4749afe3c004e5f050a342fe453e7a305ee07fed
Kayomn <kayomn@kayomn.net>
2021-01-07 01:13:58 +0000

    Add OS-level multithreading support

--- ona/engine/header/os.hpp
+++ ona/engine/header/os.hpp
@@ -1,11 +1,44 @@
 
 namespace Ona::Engine {
-	class ThreadPool : public Object {
-		public:
-		virtual void Enqueue() = 0;
+	using Task = Callable<void()>;
 
-		virtual void Execute() = 0;
+	class ThreadServer : public Object {
+		public:
+		/**
+		 * Executes `task` asynchronously.
+		 */
+		virtual void Execute(Task const & task) = 0;
+
+		/**
+		 * Initializes all worker threads and begins asynchronous processing of any pending tasks
+		 * executed prior to, or after, being called.
+		 *
+		 * Returns `true` if the `ThreadServer` was started successfully, otherwise `false`.
+		 */
+		virtual bool Start() = 0;
+
+		/**
+		 * Kills all worker threads and halts the `ThreadServer`, pausing any task in the work
+		 * queue.
+		 */
+		virtual void Stop() = 0;
+
+		/**
+		 * Halts the calling thread until all tasks running on the `ThreadServer` conclude
+		 * execution.
+		 */
+		virtual void Wait() = 0;
 	};
+
+	/**
+	 * Loads a platform-specific `ThreadServer` instance into memory.
+	 *
+	 * `hardwarePriority` specifies what percentage of access to concurrency hardware will be
+	 * requested by the process as a value between `0` and `1`. A value of `1` or greater will try
+	 * to request access to all concurrency hardware, and will likely cause context switching on any
+	 * platform that implements its own process scheduler.
+	 */
+	ThreadServer * LoadThreadServer(float hardwarePriority);
 
 	/**
 	 * Attempts to load access to the operating system filesystem as a `FileServer`.
--- ona/engine/source/linux.cpp
+++ ona/engine/source/linux.cpp
@@ -4,6 +4,117 @@
 #include <fcntl.h>
 
 namespace Ona::Engine {
+	ThreadServer * LoadThreadServer(float hardwarePriority) {
+		static class PosixThreadServer final : public ThreadServer {
+			private:
+			pthread_cond_t taskCondition;
+
+			pthread_mutex_t taskMutex;
+
+			DynamicArray<pthread_t> threads;
+
+			PackedQueue<Task> tasks;
+
+			AtomicU32 taskCount;
+
+			public:
+			PosixThreadServer(
+				float hardwarePriority
+			) :
+				tasks{DefaultAllocator()},
+
+				threads{
+					DefaultAllocator(),
+					static_cast<size_t>(sysconf(_SC_NPROCESSORS_ONLN) * hardwarePriority)
+				}
+			{
+
+			}
+
+			void Execute(Task const & task) override {
+				pthread_mutex_lock(&this->taskMutex);
+				this->tasks.Enqueue(task);
+				pthread_mutex_unlock(&this->taskMutex);
+				// Signal to the listener that there's work to be done.
+				pthread_cond_signal(&this->taskCondition);
+			}
+
+			bool Start() override {
+				static auto worker = [](void * arg) -> void * {
+					auto threadServer = reinterpret_cast<PosixThreadServer *>(arg);
+
+					pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, nullptr);
+					pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, nullptr);
+
+					for (;;) {
+						// Halt all but the first worker thread at this impasse so they
+						// don't fight over access to the list, letting only one of the
+						// threads proceed.
+						pthread_mutex_lock(&threadServer->taskMutex);
+
+						while (threadServer->tasks.Count() == 0) {
+							// While there's no work, just listen for some and wait.
+							pthread_cond_wait(
+								&threadServer->taskCondition,
+								&threadServer->taskMutex
+							);
+						}
+
+						// Ok, work acquired - let the next thread in on the action and let
+						// this one deal with the task it has acquired.
+						Task task = threadServer->tasks.Dequeue();
+
+						pthread_mutex_unlock(&threadServer->taskMutex);
+						task.Invoke();
+					}
+
+					// Because pthreads return void pointers (in case you want to return
+					// some product from the operation).
+					return nullptr;
+				};
+
+				if (
+					(pthread_cond_init(&this->taskCondition, nullptr) == 0) &&
+					(pthread_mutex_init(&this->taskMutex, nullptr) == 0)
+				) {
+					size_t threadsInitialized = 0;
+
+					this->taskCount.Store(0);
+
+					for (size_t i = 0; i < this->threads.Length(); i += 1) {
+						// TODO: Better names for threads. Keep in mind that max name length is 16.
+						if (
+							(pthread_create(&this->threads.At(i), nullptr, worker, this) == 0) &&
+							(pthread_setname_np(this->threads.At(i), "ona.thread[0]") == 0)
+						) {
+							threadsInitialized += 1;
+						}
+					}
+
+					return (threadsInitialized == this->threads.Length());
+				}
+
+				return false;
+			}
+
+			void Stop() override {
+				// Signal exit and wait for all threads to end.
+				for (size_t i = 0; i < this->threads.Length(); i += 1) {
+					pthread_cancel(this->threads.At(i));
+				}
+
+				pthread_mutex_destroy(&this->taskMutex);
+				pthread_cond_destroy(&this->taskCondition);
+			}
+
+			void Wait() override {
+				while (this->taskCount.Load() != 0);
+			}
+		} threadServer = {hardwarePriority};
+
+		return &threadServer;
+	}
+
 	FileServer * LoadFilesystem() {
 		static auto userdataToHandle = [](void * userdata) {
 			return static_cast<int>(reinterpret_cast<size_t>(userdata));
@@ -24,7 +135,7 @@ namespace Ona::Engine {
 			return 0;
 		};
 
-		static class : public FileServer {
+		static class FileSystem final : public FileServer {
 			public:
 			bool CheckFile(String const & filePath) override {
 				return (access(filePath.ZeroSentineled().Chars().pointer, F_OK) != -1);
