dedb029a7f3a366263c53d403be26629ed8bdb38
Kayomn <kayomn@kayomn.net>
2021-02-12 23:55:04 +0000

    Tidy up build script

--- build.py
+++ build.py
@@ -7,88 +7,23 @@ import json
 compiler = "clang++"
 version = "-std=c++17"
 output_dir = "output"
-common_dir = "common"
-engine_dir = "engine"
 modules_dir = "modules"
 
 if (not os.path.exists(output_dir)):
 	os.mkdir(output_dir)
-
-if (not os.path.exists(common_dir)):
-	os.mkdir(common_dir)
-
-if (not os.path.exists(engine_dir)):
-	os.mkdir(engine_dir)
 
 if (not os.path.exists(modules_dir)):
 	os.mkdir(modules_dir)
 
 root_path = os.getcwd()
 output_path = os.path.abspath(output_dir)
-common_path = os.path.abspath(common_dir)
-engine_path = os.path.abspath(engine_dir)
-modules_path = os.path.abspath(modules_dir)
-generated_file_path = os.path.join(output_path, "generated.cpp")
 compile_command = [compiler, version, ("-I" + os.path.abspath(".")), "-c", "-g"]
+needs_rebuild = False
 
-# Compile common library.
-output_file_path = os.path.join(output_path, "libcommon.a")
-output_file_mod_time = (os.path.getmtime(output_file_path) if os.path.exists(output_file_path) else 0)
-needs_rebuild = (os.path.getmtime(os.path.join(root_path, "common.hpp")) > output_file_mod_time)
-source_file_names = []
-object_file_names = []
-
-library_file_paths = [output_file_path]
-
-os.chdir(common_path)
-
-for file_name in os.listdir(common_path):
-	if (file_name.endswith(".cpp")):
-		object_file_name = (os.path.splitext(file_name)[0] + ".o")
-
-		source_file_names.append(file_name)
-		object_file_names.append(object_file_name)
-
-		if (not needs_rebuild):
-			needs_rebuild |= (
-				(not os.path.exists(object_file_name)) or
-				(os.path.getmtime(file_name) > os.path.getmtime(object_file_name))
-			)
-	elif (file_name.endswith(".hpp")):
-		needs_rebuild |= (os.path.getmtime(file_name) > output_file_mod_time)
-
-if (needs_rebuild):
-	print("Building common...")
-	subprocess.call(compile_command + source_file_names)
+def link_lib(object_file_names, output_file_path):
 	subprocess.call(["ar", "rcs", output_file_path] + object_file_names)
 
-output_file_path = os.path.join(output_path, "engine")
-output_file_mod_time = (os.path.getmtime(output_file_path) if os.path.exists(output_file_path) else 0)
-needs_rebuild |= (os.path.getmtime(os.path.join(root_path, "engine.hpp")) > output_file_mod_time)
-source_file_names = []
-object_file_names = []
-
-os.chdir(engine_path)
-
-for file_name in os.listdir(engine_path):
-	if (file_name.endswith(".cpp")):
-		object_file_name = (os.path.splitext(file_name)[0] + ".o")
-
-		source_file_names.append(file_name)
-		object_file_names.append(object_file_name)
-
-		if (not needs_rebuild):
-			needs_rebuild |= (
-				(not os.path.exists(object_file_name)) or
-				(os.path.getmtime(file_name) > os.path.getmtime(object_file_name))
-			)
-	elif (file_name.endswith(".hpp")):
-		needs_rebuild |= (os.path.getmtime(file_name) > output_file_mod_time)
-
-if (needs_rebuild):
-	print("Building engine...")
-	subprocess.call(compile_command + source_file_names)
-
+def link_exe(object_file_names, output_file_path):
 	subprocess.call([
 		compiler,
 		version,
@@ -100,3 +35,48 @@ if (needs_rebuild):
 		"-lGLEW",
 		"-lpthread"
 	] + object_file_names + library_file_paths)
+
+def build_component(component_name: str, output_file_path: str, linker) -> bool:
+	global needs_rebuild
+	component_path = os.path.join(root_path, component_name)
+
+	if (not os.path.exists(component_path)):
+		os.mkdir(component_path)
+
+	header_extension = ".hpp"
+	output_file_mod_time = (os.path.getmtime(output_file_path) if os.path.exists(output_file_path) else 0)
+	needs_rebuild |= (os.path.getmtime(component_path + header_extension) > output_file_mod_time)
+	source_file_names = []
+	object_file_names = []
+
+	os.chdir(component_path)
+
+	for file_name in os.listdir(component_path):
+		if (file_name.endswith(".cpp")):
+			object_file_name = (os.path.splitext(file_name)[0] + ".o")
+
+			source_file_names.append(file_name)
+			object_file_names.append(object_file_name)
+
+			if ((not needs_rebuild) and (
+				(not os.path.exists(object_file_name)) or
+				(os.path.getmtime(file_name) > os.path.getmtime(object_file_name))
+			)):
+				needs_rebuild = True
+		elif (file_name.endswith(header_extension)):
+			if ((not needs_rebuild) and (os.path.getmtime(file_name) > output_file_mod_time)):
+				needs_rebuild = True
+
+	if (needs_rebuild):
+		print("Building %s..." % component_name)
+		subprocess.call(compile_command + source_file_names)
+		linker(object_file_names, output_file_path)
+
+	os.chdir(root_path)
+
+# Compile common library.
+output_file_path = os.path.join(output_path, "libcommon.a")
+library_file_paths = [output_file_path]
+
+build_component("common", output_file_path, link_lib)
+build_component("engine", os.path.join(output_path, "engine"), link_exe)
