3b4570623019539d022b8bc5a93fee119330c3c1
Kayomn <kayomn@kayomn.net>
2021-02-13 23:34:30 +0000

    Implement power function

--- common/math.cpp
+++ common/math.cpp
@@ -4,4 +4,8 @@ namespace Ona {
 	float Floor(float const value) {
 		return __builtin_floorf(value);
 	}
+
+	float Pow(float const value, float const exponent) {
+		return __builtin_pow(value, exponent);
+	}
 }
